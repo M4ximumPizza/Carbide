e8de4fa3fac5f0e69701074efbd6208422364cc0
M4ximumpizza <LWabernathy@outlook.com>
2025-04-18 23:28:42 -0700

    Move Main.java to NativeTest in the test directory.

---
+++ src/test/java/natives/NativeTest.java
@@ -0,0 +1,11 @@
+package natives;
+
+import mi.m4x.carbide.natives.Architecture;
+import mi.m4x.carbide.natives.OperatingSystems;
+
+public class NativeTest {
+    public static void main(String[] args) {
+        System.out.printf(Architecture.get() + "\n");
+        System.out.printf(OperatingSystems.get() + "\n");
+    }
+}
