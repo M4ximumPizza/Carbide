f3d7175442e28bec3275656602b1a0996a3b1758
M4ximumpizza <LWabernathy@outlook.com>
2025-05-03 18:56:34 -0700

    Added the verifying for number counting inside the test files.

--- rive/src/test/java/x86/RegisterTest.java
+++ rive/src/test/java/x86/RegisterTest.java
@@ -3,21 +3,36 @@ package x86;
 import mi.m4x.rive.registers.Register;
 
 import java.lang.reflect.Field;
+import java.util.*;
 
 public class RegisterTest {
     public static void main(String[] args) {
         try {
             // Get all declared fields in the Register class
             Field[] fields = Register.class.getDeclaredFields();
+            Map<Integer, String> idToName = new TreeMap<>(); // Automatically sorted by ID
 
             for (Field field : fields) {
                 // Check if the field is of type int
                 if (field.getType() == int.class) {
-                    // Make the field accessible and retrieve its value
                     field.setAccessible(true);
                     int value = field.getInt(null);
-                    System.out.println("Name: " + field.getName() + ", ID: " + value);
+                    idToName.put(value, field.getName());
                 }
+            }
+
+            System.out.println("Total int fields: " + idToName.size());
+
+            int expected = -1;
+            for (Map.Entry<Integer, String> entry : idToName.entrySet()) {
+                int actual = entry.getKey();
+                if (expected == -1) {
+                    expected = actual;
+                } else if (actual != expected + 1) {
+                    System.err.println("Gap or disorder detected between ID " + expected + " and " + actual);
+                }
+                System.out.println("Name: " + entry.getValue() + ", ID: " + actual);
+                expected = actual;
             }
         } catch (IllegalAccessException e) {
             e.printStackTrace();
