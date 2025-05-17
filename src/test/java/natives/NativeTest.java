9a82bd641789d9482d1f50c1feacfc4fdf92b831
M4ximumPizza <109777745+M4ximumPizza@users.noreply.github.com>
2025-05-16 19:44:41 -0700

    Improve OS detection logic and refactor enum for clarity and maintainability

--- src/test/java/natives/NativeTest.java
+++ src/test/java/natives/NativeTest.java
@@ -6,6 +6,6 @@ import mi.m4x.carbide.natives.OperatingSystems;
 public class NativeTest {
     public static void main(String[] args) {
         System.out.printf(Architecture.get() + "\n");
-        System.out.printf(OperatingSystems.get() + "\n");
+        System.out.printf(OperatingSystems.detect() + "\n");
     }
 }
